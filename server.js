5317659314164e8228d83b49ae1d894ce19c1ee3
Jana Laney <Jana.Laney@gmail.com>
2020-04-15 10:21:45 -0600

    deleted a comment

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ require('./routes/apiRoutes/')(app);
 //pass the html file into the routes folder
 require('./route/htmlRoutes')(app);
 
-start our Server
+
 app.list(PORT, function () {
     console.log("App listening on PORT: " + PORT)
 })
